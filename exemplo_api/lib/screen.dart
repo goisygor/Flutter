681286ee015540034d652c45adf1e8a05ad501dc
goisygor <ygorgoissenai@gmail.com>
2024-05-14 20:48:48 -0300

    ok

--- exemplo_api/lib/screen.dart
+++ exemplo_api/lib/screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-
 import 'service.dart';
 
 class WeatherScreen extends StatefulWidget {
-  const WeatherScreen({super.key});
+  const WeatherScreen({Key? key}) : super(key: key);
 
   @override
   State<WeatherScreen> createState() => _WeatherScreenState();
@@ -11,26 +10,24 @@ class WeatherScreen extends StatefulWidget {
 
 class _WeatherScreenState extends State<WeatherScreen> {
   final WeatherService _weatherService = WeatherService(
-    apiKey: 'b9ebe666087f299f5e2aad3a03d093b6', // Chave de API para acesso à API de previsão do tempo.
-    baseUrl: 'https://api.openweathermap.org/data/2.5', // URL base da API de previsão do tempo.
+    apiKey: '681126f28e7d6fa3a7cfe0da0671e599',
+    baseUrl: 'https://api.openweathermap.org/data/2.5',
   );
 
-  late Map<String, dynamic> _weatherdata;
-  late TextEditingController _cityController;
+  Map<String, dynamic> _weatherData = {};
+  TextEditingController _cityController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    _weatherdata = new Map<String, dynamic>();
-    _cityController = TextEditingController();
   }
 
-  Future<void> _fecthWeatherData(String city) async {
+  Future<void> _fetchWeatherData(String city) async {
     try {
       final weatherData = await _weatherService.getWeather(city);
       setState(() {
-        _weatherdata = weatherData;
+        _weatherData = weatherData;
       });
     } catch (e) {
       print(e);
@@ -40,32 +37,43 @@ class _WeatherScreenState extends State<WeatherScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Exemplo weather-API"),
-      ),
+      appBar: AppBar(title: const Text("Exemplo Weather-API")),
       body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            TextField(
-              controller: _cityController,
-              decoration: InputDecoration(
-                labelText: "",
+        padding: EdgeInsets.all(12),
+        child: Center(
+          child: Form(
+            key: formKey,
+            child: Column(children: [
+              TextFormField(
+                controller: _cityController,
+                decoration: const InputDecoration(labelText: "Digite a Cidade"),
+                validator: (value) {
+                  if (value!.trim().isEmpty) {
+                    return "Insira a Cidade";
+                  }
+                  return null;
+                },
               ),
-              onChanged: (value) {},
-            ),
-            SizedBox(
-                height:
-                    16.0), 
-            ElevatedButton(
-              onPressed: () {
-                String cityNome = _cityController.text;
-
-                _fecthWeatherData(cityNome);
-              },
-              child: Text('Buscar'),
-            ),
-          ],
+              ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    _fetchWeatherData(_cityController.text);
+                  }
+                },
+                child: const Text("Buscar"),
+              ),
+              //mostrar informações
+              if (_weatherData.containsKey('main'))
+                Column(
+                  children: [
+                    Text(
+                      "Temperatura: ${_weatherData['main']['temp'] - 273} °C ",
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+            ]),
+          ),
         ),
       ),
     );
